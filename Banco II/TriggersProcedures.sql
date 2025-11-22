dafe902041c92f3fe3be0888accac02ace387f67
DouglasSierota <douglasgabrielsierota@gmail.com>
2025-11-22 13:37:04 -0300

    Adicionado Triggers Procedures

---
+++ Banco II/TriggersProcedures.sql
@@ -0,0 +1,35 @@
+
+--  TRIGGERS E PROCEDURES 
+
+
+CREATE TABLE IF NOT EXISTS log_auditoria (
+    id SERIAL PRIMARY KEY,
+    tabela VARCHAR(50),
+    operacao VARCHAR(10),
+    data_hora TIMESTAMP DEFAULT NOW()
+);
+
+
+CREATE OR REPLACE FUNCTION registrar_auditoria()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO log_auditoria (tabela, operacao, data_hora)
+    VALUES (TG_TABLE_NAME, TG_OP, NOW());
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER trg_auditoria_venda
+AFTER INSERT OR UPDATE OR DELETE ON venda
+FOR EACH ROW
+EXECUTE FUNCTION registrar_auditoria();
+
+-----------------------------
+
+CREATE OR REPLACE TRIGGER trg_auditoria_maquina
+AFTER INSERT OR UPDATE OR DELETE ON maquina
+FOR EACH ROW
+EXECUTE FUNCTION registrar_auditoria();
+
+
